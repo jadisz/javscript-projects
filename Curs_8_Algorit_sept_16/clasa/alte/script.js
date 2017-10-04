a2c1428ff71cd45ddd4b5309e2f03cd39b3ef813
zamzu <upriscila@outlook.com>
2017-10-04 10:43:57 +0300

    cursuri

---
+++ Curs_8_Algorit_sept_16/clasa/alte/script.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 20;
+var max  = undefined;
+if (a < b){
+    max = b;
+} 
+else {
+    max = a;
+}
+console.log (max);
---
+++ Curs_9_Alg2_sept_23/alte/functieadaugaelev.js
@@ -0,0 +1,19 @@
+function adaugareElev(){
+
+
+}
+
+
+
+ <div id = "adaugare_elev" class = "box">
+     <span> Nume: </span>
+
+     <input type = "text" id = "nume_elev" autofocus
+
+onkeypress = "if(event.Keycode==13) {document.getElementById('btnAdaugareElev').click();}
+
+
+
+<input type = "button" value  = "adauga elev" id = "btnAdaugareElev" onlick="adaugareElev(); " />
+
+
